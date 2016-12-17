3434ac8c340bb287282068b64830540d1ee4e08c
eggp <gabor.harangozo@eggproject.hu>
2016-12-17 16:25:54 +0100

    bugfix: socketIO auto reconnect is have bug when server restart ... replace my tested manual reconnect code

--- lib/angular2/shared/sockets/connections.ts
+++ lib/angular2/shared/sockets/connections.ts
@@ -13,44 +13,56 @@ import { AccessToken } from '../models';
 **/
 @Injectable()
 export class SocketConnections {
-  private connections: any = {};
-  private configured: boolean  = false;
-  constructor(@Inject(SocketDriver) private driver: SocketDriver) {}
-  getHandler(url: string, token: AccessToken) {
-    if (!this.connections[url]) {
-      console.log('Creating a new connection with: ', url);
-      let config: any = { log: false, secure: false, forceWebsockets: true };
-      this.connections[url] = this.driver.connect(url, config);
-      this.connections[url].on('connect', () => {
-        if (!this.configured) 
-        this.setupConnection(url, token, config);
-      });
-      let forceConfig: any = setInterval(() => {
-        if (!this.configured && this.connections[url].connected) {
-          console.info('Forcing IO Configuration');
-          this.setupConnection(url, token, config);
-          clearInterval(forceConfig);
-        } else if (this.configured) {
-          clearInterval(forceConfig);
-        }
-      }, 1000)
-    } else {
-      console.log('Reusing existing connection: ', url);
-    }
-    return this.connections[url];
+  private _connections: any = {};
+  private _socketReconnectTimeout: any;
+
+  constructor(@Inject(SocketDriver) private driver: SocketDriver) {
   }
 
-  private setupConnection(url: string, token: AccessToken, config: any): void {
-    this.configured = true;
-    console.log('Connected to %s', url);
-    if(token.id) {
-      this.connections[url].emit('authentication', token);
+  getHandler(url: string, token: AccessToken) {
+    if (!this._connections[url]) {
+      let config: any = {log: false, secure: false, forceWebsockets: true, reconnection: false, forceNew: true};
+
+      this._connections[url] = this.driver.connect(url, config);
+      let socket: any/*SocketIOClient.Socket*/ = this._connections[url];
+      socket.on('connect_error', this._handlerErrorAndReconnect("connect_error", socket));
+      socket.on('connect_timeout', this._handlerErrorAndReconnect("connect_timeout", socket));
+      socket.on('reconnect_error', this._handlerErrorAndReconnect("reconnect_error", socket));
+      socket.on('reconnect_failed', this._handlerErrorAndReconnect("reconnect_failed", socket));
+      socket.on('disconnect', this._handlerErrorAndReconnect("disconnect", socket));
+      socket.on('connect', () => {
+        console.log("Socket connected");
+        if (token.id) {
+          console.log("Socket emit authentication");
+          socket.emit('authentication', token);
+        }
+      });
+      socket.on('unauthorized', (res: any) => console.error('Unauthenticated', res));
+      setInterval(() => {
+        if (socket.connected) {
+          socket.emit('lb-ping');
+        }
+      }, 15000);
+      socket.on('lb-pong', (data: any) => console.info('Heartbeat: ', data));
     }
-    this.connections[url].on('unauthorized', (res: any) => console.error('Unauthenticated', res));
-    setInterval(() => this.connections[url].emit('lb-ping'), 15000);
-    this.connections[url].on('lb-pong', (data: any) => console.info('Heartbeat: ', data));
-    this.connections[url].on('disconnect', (data: any) => {
-      console.info('Unexpected disconnection from IO - Socket IO will try to reconnect');
-    });
+
+    return this._connections[url];
+  }
+
+  private _handlerErrorAndReconnect(name, socket) {
+    return (error?) => {
+      console.log(`Socket ${name}`, error);
+      this._socketReconnect(socket);
+    };
+  }
+
+  private _socketReconnect(socket) {
+    if (this._socketReconnectTimeout != null) {
+      clearTimeout(this._socketReconnectTimeout);
+      this._socketReconnectTimeout = null;
+    }
+    this._socketReconnectTimeout = setTimeout(() => {
+      socket.connect();
+    }, 4000);
   }
 }
